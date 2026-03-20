6d92fddc951d09129a30c63e078b580676f00f79
smriti123 <a_smriti@yahoo.com>
2026-03-20 22:06:44 +0100

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,8 +1,8 @@
 import { motion } from "framer-motion";
 import heroPortrait   from "@/assets/pic1.jpg";
-import heroHimalaya  from "@/assets/pic2.jpg";
+import heroHimalaya  from "@/assets/pic4.jpg";
 import heroFeet      from "@/assets/pic3.jpg";
-import heroShiva     from "@/assets/pic4.jpg";
+import heroShiva     from "@/assets/pic2.jpg";
 import heroSignature from "@/assets/pic5.jpg";
 
 const HeroSection = () => {
