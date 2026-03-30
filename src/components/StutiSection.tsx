2cbcc2933bef4244ed29fb641983ba8222b4433c
smriti123 <a_smriti@yahoo.com>
2026-03-30 11:26:55 +0200

    Update StutiSection.tsx

--- src/components/StutiSection.tsx
+++ src/components/StutiSection.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { ChevronDown, ChevronUp, ChevronLeft, ChevronRight } from "lucide-react";
 import SectionHeading from "./SectionHeading";
-import GyannishthaCard from "@/components/GyannishthaCard";
+import GyannishthaCard from "@/components/GyannisthaCard";
 
 const stutiNames = [
   "ॐ ॐकाराय नमः ।",
